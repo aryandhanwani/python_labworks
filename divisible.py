cd81b509b7c75fb10e493d87e73cc513a42476da
Aryan Dhanwani <aryandhanwani11@gmail.com>
2025-02-11 00:25:23 +0530

    check the number is divisible 2 ,3 or both 2 & 3

---
+++ divisible.py
@@ -0,0 +1,7 @@
+for i in range(1,51):
+    if(i%2==0 and i%3==0):
+        print(i,"-the number is divisible by both 2 and 3")
+    elif(i%2==0):
+        print(i,"-the number is divisible by 2")
+    elif(i%3==0):
+        print(i,"-the number is divisble by 3")
